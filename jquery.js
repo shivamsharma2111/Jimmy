754ac53bafb0e6fffed3d9f130d0a5efbc27211d
shivamsharma2111 <120738656+shivamsharma2111@users.noreply.github.com>
2023-10-22 20:35:39 +0530

    added jimmi

---
+++ jquery.js
@@ -0,0 +1,99 @@
+var a,b,c
+a=10;
+b=20;
+c=a+b;
+document.getElementById("abc").innerHTML=c;
+
+
+
+
+
+
+var m,b,a
+m=20;
+b=30;
+a=b-m;
+document.getElementById("mba").innerHTML=a;
+
+
+
+
+
+
+var f,u,n
+f=14;
+u=2;
+n=f*u;
+document.getElementById("fun").innerHTML=n;
+
+
+
+
+
+
+function number(p1,p2)
+{return p1*p2;}
+document.getElementById("name").innerHTML=number(12,5);
+
+
+
+
+
+
+
+var s=a1(8,2);
+document.getElementById("done").innerHTML=s;
+function a1(p1,p2)
+{return p1/p2;}
+
+
+
+
+
+
+
+
+var a,b,c;
+a=4;
+b=2;
+c=4;
+document.getElementById("tv").innerHTML
+=(a==b)+"br "+(a==c)+" br"+(b==c);
+
+
+
+
+
+
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+
+
+
+
+
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "click on the button.";
+}
+
+
+
+
+
+
+
+
+function myFunction2() {
+  document.getElementById("dream").innerHTML = .style."JavaScript uygdfyvahiuehg qwiyhfuhf arwighun eeuiahggn fuhguangf sguhgunw uhaung iuhgungug ugs The functiis invoked when a butn is clicked";
+}
+
+
+
+
+
+
